c2f67fe435bf4b4f60c9efdbef72fb88916603f8
Ali Gheisar <ali.gheisar.dev@gmail.com>
2026-07-05 12:26:49 +0330

    make main code more readable.

--- src/main.ts
+++ src/main.ts
@@ -2,68 +2,31 @@ import { createContainer } from "@/containers";
 import { createCLI } from "@/presentation/cli";
 import { launchTUI } from "@/presentation/tui";
 
-const container = createContainer();
-
-const args = process.argv.slice(2);
-const hasSubcommand = [
-  "scan",
-  "run",
-  "cache",
-  "completion",
-  "--help",
-  "-h",
-  "--version",
-  "-v",
-].some((cmd) => args.includes(cmd));
-
 function expandPath(p: string): string {
-  if (p.startsWith("~")) {
-    return p.replace("~", process.env.HOME ?? "");
-  }
-  return p;
+  return p.startsWith("~") ? p.replace("~", process.env.HOME ?? "") : p;
 }
 
-if (!hasSubcommand) {
-  const folderArg = args[0];
-  const isPath =
-    folderArg &&
-    (folderArg.startsWith("/") ||
-      folderArg.startsWith("./") ||
-      folderArg.startsWith("../") ||
-      folderArg.startsWith("~"));
+const container = createContainer();
+const program = createCLI(
+  container.scannerService,
+  container.runService,
+  container.cacheService,
+  [...container.producers.keys()],
+);
 
-  if (isPath) {
-    launchTUI({
-      filesystem: container.filesystem,
-      initialFolder: expandPath(folderArg),
-      libraryRepository: container.libraryRepository,
-      producerNames: [...container.producers.keys()],
-      producers: container.producers,
-      scannerService: container.scannerService,
-      tagWriter: container.tagWriter,
-    });
-  } else if (args.length === 0) {
-    launchTUI({
-      filesystem: container.filesystem,
-      libraryRepository: container.libraryRepository,
-      producerNames: [...container.producers.keys()],
-      producers: container.producers,
-      scannerService: container.scannerService,
-      tagWriter: container.tagWriter,
-    });
-  } else {
-    createCLI(
-      container.scannerService,
-      container.runService,
-      container.cacheService,
-      [...container.producers.keys()],
-    ).parse();
-  }
-} else {
-  createCLI(
-    container.scannerService,
-    container.runService,
-    container.cacheService,
-    [...container.producers.keys()],
-  ).parse();
-}
+program.action((_, command) => {
+  const folder = command.args[0];
+  const isPath = folder && /^(\/|\.\/|\.\.\/|~)/.test(folder);
+
+  launchTUI({
+    filesystem: container.filesystem,
+    initialFolder: isPath ? expandPath(folder) : undefined,
+    libraryRepository: container.libraryRepository,
+    producerNames: [...container.producers.keys()],
+    producers: container.producers,
+    scannerService: container.scannerService,
+    tagWriter: container.tagWriter,
+  });
+});
+
+program.parse();
